9eb8ef3f31cfccf0c5c96abb5dacc270cbf1645e
Regina Bonifacio <rejinimikasi06@gmail.com>
2025-01-31 17:35:48 +0800

    Implemented BruteForce class for factorization

---
+++ decryption_algorithms.py
@@ -0,0 +1,11 @@
+class BruteForce:
+    @staticmethod
+    def decrypt(n: int, timeout: int = 60) -> Optional[Tuple[int, int]]:
+        """Brute-force factorization"""
+        start_time = time.time()
+        i = 2
+        while i * i <= n and time.time() - start_time < timeout:
+            if n % i == 0:
+                return (i, n // i)
+            i += 1
+        return None
