3f6de2749a828b8e199443d6b5094c153ee5408b
Regina Bonifacio <rejinimikasi06@gmail.com>
2025-01-31 17:37:59 +0800

    Added EulersFactorization class for factorization using Euler's method

--- decryption_algorithms.py
+++ decryption_algorithms.py
@@ -6,7 +6,7 @@ from sympy import isprime, gcd
 
 class BruteForce:
     @staticmethod
-    def decrypt(n: int, timeout: int = 60) -> Optional[Tuple[int, int]]:
+    def decrypt(n: int, timeout: int = 30) -> Optional[Tuple[int, int]]:
         """Brute-force factorization"""
         start_time = time.time()
         i = 2
@@ -14,4 +14,22 @@ class BruteForce:
             if n % i == 0:
                 return (i, n // i)
             i += 1
+        return None
+
+class EulersFactorization:
+    @staticmethod
+    def decrypt(n: int, timeout: int = 30) -> Optional[Tuple[int, int]]:
+        """Euler's factorization method"""
+        start_time = time.time()
+        a = math.isqrt(n)
+        
+        while time.time() - start_time < timeout:
+            b2 = a * a - n
+            if b2 < 0:
+                a += 1
+                continue
+            b = math.isqrt(b2)
+            if b * b == b2:
+                return (a - b, a + b)
+            a += 1
         return None
