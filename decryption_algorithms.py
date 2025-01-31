44b9e831b3084763ee7edbe0b5a671b01d3fe503
Regina Bonifacio <rejinimikasi06@gmail.com>
2025-01-31 17:36:49 +0800

    Added necessary libraries for decryption algorithms

--- decryption_algorithms.py
+++ decryption_algorithms.py
@@ -1,3 +1,9 @@
+import math
+import random
+import time
+from typing import Tuple, Optional
+from sympy import isprime, gcd
+
 class BruteForce:
     @staticmethod
     def decrypt(n: int, timeout: int = 60) -> Optional[Tuple[int, int]]:
